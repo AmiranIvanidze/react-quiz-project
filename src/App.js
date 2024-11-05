82e5613b5ae46cd65f36f7a0a170960636bce1a0
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-05 19:38:35 +0400

    set-up useReducer for data handling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import './App.css';
 import Header from './Header';
 import Main from './Main';
+
+const initialState = { 
+  questions: [],
+  status: 'loading',
+  
+ }
+
+function reducer (state, action) {
+
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state, 
+        questions: action.payload,
+        status: "ready"
+      };
+    case "dataFailed":
+      return {
+        ...state, 
+        status: "error"
+      };
+    default:
+      throw new Error("Action is unknown!");
+  }
+
+}
 function App() {
 
-  const [state, dispatch] = useReducer()
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(function(){
-    fetch('http://localhost:8000/questions/').then(res =>  res.json()).then(data => console.log(data)).catch(err => console.log(err))
+    fetch('http://localhost:8000/questions/')
+    .then(res =>  res.json())
+    .then(data => dispatch({type:'dataReceived', payload: data}))
+    .catch(err => dispatch({type:"dataFailed"}))
   }, [])
   return (
     <>
