41ca1a450343dc78729345749f453750dad919b9
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-05 19:08:28 +0400

    Create Main.js Component And Start up App.js for quizz app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import DateCounter from './DateCounter'
+import Header from './Header';
+import Main from './Main';
 function App() {
   return (
     <>
-      <DateCounter/>
+      <div className="app">
+       <Header/>
+       <Main>
+        <p>1/15</p>
+        <p>Question!</p>
+       </Main>
+      </div>
     </>
   );
 }
