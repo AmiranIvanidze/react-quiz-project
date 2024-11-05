d7bfccf69a229bb049b2b1854f4141dabf4efbc8
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-05 19:22:43 +0400

    Install npm json-server and set up data/questions.json files json server command in package.json scripts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Main from './Main';
 function App() {
+
+  const [state, dispatch] = useReducer()
+
+  useEffect(function(){
+    fetch('http://localhost:8000/questions/').then(res =>  res.json()).then(data => console.log(data)).catch(err => console.log(err))
+  }, [])
   return (
     <>
       <div className="app">
