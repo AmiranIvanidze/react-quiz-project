4b56cb8dfec1d3971c9efb603722f0140aeb9c42
AmiranIvanidze <a.ivanidz3@gmail.com>
2024-11-06 21:22:25 +0400

    Increment Progress Component In App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from './Error';
 import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
+import Progress from './Progress';
 
 const initialState = { 
   questions: [],
@@ -55,8 +56,9 @@ function reducer (state, action) {
 }
 function App() {
 
-  const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState)
+  const [{questions, status, index, answer, points}, dispatch] = useReducer(reducer, initialState)
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, curr) => prev + curr.points, 0)
   useEffect(function(){
     fetch('http://localhost:8000/questions/')
     .then(res =>  res.json())
@@ -73,6 +75,7 @@ function App() {
         { status === 'ready'  &&  <StartScreen num={numQuestions} dispatch={dispatch}/> }
         { status === 'active' && (
          <>
+          <Progress index={index} numQuestions={questions.length} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer} />
            <Question dispatch={dispatch} answer={answer} question={questions[index]}/>
            <NextButton dispatch={dispatch} answer={answer}/>
          </>
